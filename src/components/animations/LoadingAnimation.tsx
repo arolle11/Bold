75a0712d8f3173a7ee26796d501d32b6dfe220a8
Arolle11 <arollefona11@gmail.com>
2025-10-20 22:58:58 +0100

    updates

--- src/components/animations/LoadingAnimation.tsx
+++ src/components/animations/LoadingAnimation.tsx
@@ -1,7 +1,10 @@
 const LoadingAnimation = () => {
   return (
     <div className=" cursor-pointer flex justify-center items-center md:min-w-[500px] min-w-[300px] min-h-[500px] bg-[#f1f1f1] text-white gap-8 p-8 rounded-md">
-      ß
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolores
+      magni corrupti obcaecati ab dignissimos repellat velit qui. Provident,
+      quia similique? Quia dolor repellendus ipsam odio ea voluptatibus adipisci
+      nihil!
     </div>
   );
 };
